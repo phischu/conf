cc626f3c4e4dffa84545b6fd7cacc13bc17236ed
Philipp Schuster <pschuster@uni-koblenz.de>
2013-09-19 11:51:39 +0200

    error handling

--- src/Conf.hs
+++ src/Conf.hs
@@ -6,41 +6,43 @@ import Pipes.Prelude (toListM)
 
 import Control.Monad.Trans.Free (FreeT,FreeF(Pure,Free),runFreeT,liftF,wrap)
 import Control.Monad.Trans.State (StateT,runStateT,modify)
+import Control.Monad.Trans.Writer (WriterT,runWriterT,tell)
 import Control.Monad.Identity (Identity,runIdentity)
 import Control.Monad.Trans (lift)
+import Data.Monoid (Monoid)
 import Control.Monad (mzero,mplus)
 
 import Data.Set (Set,insert)
 
-type Conf flag = ConfT flag Identity
+type Conf err flag = ConfT err flag Identity
 
-type ConfT flag = FreeT (ConfF flag)
+type ConfT err flag = FreeT (ConfF err flag)
 
-data ConfF flag a = Require flag a
+data ConfF err flag a = Require flag a
                   | Fork a a
-                  | Failure String
+                  | Failure err
                     deriving (Functor,Show,Read,Eq)
 
-require :: (Monad m) => flag -> ConfT flag m ()
+require :: (Monad m) => flag -> ConfT err flag m ()
 require flag = liftF (Require flag ())
 
-fork :: (Monad m) => ConfT flag m a -> ConfT flag m a -> ConfT flag m a
+fork :: (Monad m) => ConfT err flag m a -> ConfT err flag m a -> ConfT err flag m a
 fork branch1 branch2 = wrap (Fork branch1 branch2)
 
-failure :: (Monad m) => String -> ConfT flag m a
-failure message = liftF (Failure message)
+failure :: (Monad m) => err -> ConfT err flag m a
+failure err = liftF (Failure err)
 
-runConf :: (Ord flag) => Conf flag a -> Set flag -> [(a,Set flag)]
+runConf :: (Ord flag,Monoid err) => Conf err flag a -> Set flag -> ([(a,Set flag)],err)
 runConf conf flags = runIdentity (runConfT conf flags)
 
-runConfT :: (Monad m,Ord flag) => ConfT flag m a -> Set flag -> m [(a,Set flag)]
-runConfT conft flags = toListM (enumerate (runStateT (interpretConfT conft) flags))
+runConfT :: (Monad m,Ord flag,Monoid err) => ConfT err flag m a -> Set flag -> m ([(a,Set flag)],err)
+runConfT conft flags = runWriterT (toListM (enumerate (runStateT (interpretConfT conft) flags)))
 
-interpretConfT :: (Monad m,Ord flag) => ConfT flag m a -> StateT (Set flag) (ListT m) a
+interpretConfT :: (Monad m,Ord flag,Monoid err) => ConfT err flag m a -> StateT (Set flag) (ListT (WriterT err m)) a
 interpretConfT conft = do
-    nextstep <- lift (lift (runFreeT conft))
+    nextstep <- lift (lift (lift (runFreeT conft)))
     case nextstep of
         Pure a -> return a
         Free (Require flag continue) -> modify (insert flag) >> interpretConfT continue
         Free (Fork continue1 continue2) -> interpretConfT continue1 `mplus` interpretConfT continue2
-        Free (Failure _) -> lift mzero
+        Free (Failure err) -> lift (lift (tell err)) >> mzero
